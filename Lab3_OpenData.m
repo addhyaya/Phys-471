a1544557ee34291a808af0e222dccc1f8175fe6c
addhyaya <113860198+addhyaya@users.noreply.github.com>
2023-03-27 14:27:33 -0400

    Update Lab3_OpenData.m

--- Lab3_OpenData.m
+++ Lab3_OpenData.m
@@ -1,18 +1,16 @@
-
-% % wavelength = Opticalspectra(1:1101,1);
-% T = importdata('Optical_spectra.xlsx'); 
+% Import data as a numeric matrix by using the import data tool in MATLAB. I have put the workspace in my reference section too if you just want to open it and run.
+wavelength = Opticalspectra(1:1101,1);
 for i = 2 : 12
-    plot(wavelength,Opticalspectra(1:1101,i)+1.3*i);
+    plot(wavelength,Opticalspectra(1:1101,i)+1.3*i); % 1.3*i is put there so that everything is shifted up every loop. This is done only for representation.
     hold on
     grid on
     xlabel('Wavelength (nm)')
     ylabel('Normalized Absorbance')
-    set(gca,'YTick',[ ])
+    set(gca,'YTick',[ ]) % Using this to get rid of the y-axis values since they are already normalized.
     title('Optical absorption spectra')
-
-%     legend('(6,5)','(7,3)','(7,5)','(9,1)','(7,6)','(8,4)','(9,2)','(10,2)','(9,4)','(10,3)','(11,1)')
 end
 i = 0;
+% Labelling the chirality indices(n,m) for better representation.
 text(300,2.8,'(6,5)')
 text(300,2.66+1.5,'(7,3)')
 text(300,2.66+2.5,'(7,5)')
